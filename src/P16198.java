064cb8d95ab7f99bb06801a3b6f878e4fd3c7a9d
yeonnseok <ljinsk3@gmail.com>
2021-04-15 17:13:34 +0900

    P16198 energy

---
+++ src/P16198.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class P16198 {
+    
+    static int solve(ArrayList<Integer> a) {
+        int n = a.size();
+        if (n == 2) return 0;
+
+        int ans = 0;
+        for (int i = 1; i < n-1; i++) {
+            int energy = a.get(i-1)* a.get(i+1); // i번째 구슬을 제거하고 얻을 수 있는 에너지
+            ArrayList<Integer> b = new ArrayList<>(a);
+            b.remove(i); // i번째 구슬을 제거
+            energy += solve(b);
+            if (ans < energy) {
+                ans = energy;
+            }
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        ArrayList<Integer> a = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            a.add(sc.nextInt());
+        }
+        System.out.println(solve(a));
+    }
+}
